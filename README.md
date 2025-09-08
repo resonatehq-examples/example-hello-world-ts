dfed095580ad269809b9b27d332bb0c9125207ed
Cully Wakelin <cully.wakelin@resonatehq.io>
2025-09-08 12:44:57 -0600

    initial version of hello world example

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# example-hello-world-ts
+![hello world banner](/assets/hello-world-banner.png)
+
+# Classic 'Hello World!'
+
+**Resonate TypeScript SDK**
+
+Get started with a classic "Hello World!" example.
+
+This example executes `foo()` as a workflow where, `bar()`, and `baz()` are blocking steps.
+The workflow produces a greeting contributed to by each of the functions.
+
+## How to run this example
+
+This example does NOT require the use of a Resonate Server.
+
+Install dependencies:
+
+```shell
+bun install
+```
+
+Run script:
+
+```shell
+bun run index.ts
+```
---
+++ index.ts
@@ -0,0 +1,36 @@
+import { Resonate } from "@resonatehq/sdk";
+import type { Context } from "@resonatehq/sdk";
+
+const resonate = Resonate.local();
+
+function* baz(_: Context, greetee: string): Generator<any, string, any> {
+  console.log("running baz");
+  return `Hello ${greetee} from baz!`;
+}
+
+function* bar(_: Context, greetee: string): Generator<any, string, any> {
+  console.log("running bar");
+  return `Hello ${greetee} from bar!`;
+}
+
+function* foo(ctx: Context, greetee: string): Generator<any, string, any> {
+  console.log("running foo");
+  const fooGreeting = `Hello ${greetee} from foo!`;
+  const barGreeting = yield* ctx.run(bar, greetee);
+  const bazGreeting = yield* ctx.run(baz, greetee);
+  const greeting = `${fooGreeting} ${barGreeting} ${bazGreeting}`;
+  return greeting;
+}
+
+const fooR = resonate.register("foo", foo);
+
+async function main() {
+  try {
+    const result = await fooR.run("greeting-workflow-1", "World");
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+main();
